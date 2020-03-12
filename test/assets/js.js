7b83deb2033f094cee8ba2c96f5336d0e2bac572
zlatov <zlatov@ya.ru>
2020-03-12 13:41:05 +0300

    q

--- test/assets/js.js
+++ test/assets/js.js
@@ -5,7 +5,12 @@ import "./sass.scss"
 import "ilorem"
 
 // 
-// Подключение пакета из папки modules (скомпилировано bash/build.sh).
+// Подключение пакета (скомпилировано bash/build.sh).
 // 
+
+// // без настроек
+// import "imenu"
+// import "imenu/dist/css.css"
+
+// с настройками (см. test/assets/sass.scss)
 import "imenu"
-import "imenu/dist/css.css"
