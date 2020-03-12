286caea51fb015575b3547b60e783e24d7b8332f
zlatov <zlatov@ya.ru>
2020-03-12 15:22:10 +0300

    q

---
+++ test/assets/js.js
@@ -0,0 +1,16 @@
+// 
+// Дополнительные пакеты и стили для разработки и тестов.
+// 
+import "./sass.scss"
+import "ilorem"
+
+// 
+// Подключение пакета (скомпилировано bash/build.sh).
+// 
+
+// // без настроек
+// import "imenu"
+// import "imenu/dist/css.css"
+
+// с настройками (см. test/assets/sass.scss)
+import "imenu"
